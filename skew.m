2b3792b99e264fb833f03b21bf3a6ea29049e4fb
adco8621 <47127794+adco8621@users.noreply.github.com>
2019-04-08 22:23:34 -0600

    removing in use files from "old-files"

--- skew.m
+++
@@ -1,6 +0,0 @@
-function A = skew(v)
-% Takes a 3*3 vector v and converts it to a skew symmetric matrix
-A = zeros(3);
-A = diag([v(3),v(1)],-1) + diag(-[v(3),v(1)],1) + diag(v(2),2) + diag(-v(2),-2);
-end
-
